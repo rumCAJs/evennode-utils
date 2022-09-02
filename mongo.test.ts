5bbb49146ab96d519f5980c3ae00944221a9deb8
Jaromir Polena <jarda.futu@gmail.com>
2022-09-02 14:03:32 +0200

    tests fixes

--- mongo.test.ts
+++ mongo.test.ts
@@ -2,59 +2,50 @@ import { pipe } from 'fp-ts/lib/function'
 import { match as matchE } from 'fp-ts/lib/Either'
 import test from 'tape'
 import mongo from './mongo'
-import { writeFileSync } from 'fs'
 
 const { parseConf } = mongo
 
 const validEnv =
 	'{"mongo":{"hostString":"host","user":"user123","db":"dbname"}}'
 
-const notValidEnv =
-	'{"mong":{"hostString":"host","user":"user123","db":"dbname"}}'
+const notValidEnvs = [
+	'{"mong":{"hostString":"host","user":"user123","db":"dbname"}}',
+	'{"mongo":{"hostStriing":"host","user":"user123","db":"dbname"}}',
+	'{"mongo":{"hostString":"host","user":42,"db":"dbname"}}',
+]
 
-test('should retrun Either.Left<ParseError> if niot valid value is provided', (t) => {
-	t.plan(3)
-	pipe(
-		parseConf(undefined),
-		matchE(
-			(e) => t.equal((e as any)?._type, 'ParseError'),
-			() => t.fail()
-		)
-	)
+test('should return Either.Left<ParseError> if not valid JSON is provided', (t) => {
+	const data = [undefined, null, 'abc']
+	t.plan(data.length)
 
-	pipe(
-		parseConf(null),
-		matchE(
-			(e) => t.equal((e as any)?._type, 'ParseError'),
-			() => t.fail()
+	data.forEach((env) => {
+		pipe(
+			parseConf(env),
+			matchE(
+				(e) => t.equal((e as any)?._type, 'ParseError'),
+				() => t.fail()
+			)
 		)
-	)
-
-	pipe(
-		parseConf('asd'),
-		matchE(
-			(e) => t.equal((e as any)?._type, 'ParseError'),
-			() => t.fail()
-		)
-	)
+	})
 })
 
-test('should return Errors when JSON is wrong shape', (t) => {
-	t.plan(1)
-	pipe(
-		parseConf(notValidEnv),
-		matchE(
-			(e) => {
-				console.log(e)
-				writeFileSync('temp', JSON.stringify(e, null, 4))
-				t.fail()
-			},
-			(conf) => t.deepEqual(conf, JSON.parse(validEnv))
+test('should return Either.Left<Errors> when JSON has wrong shape', (t) => {
+	t.plan(notValidEnvs.length)
+
+	notValidEnvs.forEach((env) => {
+		pipe(
+			parseConf(env),
+			matchE(
+				(e) => {
+					t.ok(Array.isArray(e))
+				},
+				() => t.fail()
+			)
 		)
-	)
+	})
 })
 
-test('should return MongoConf Object if value is right', (t) => {
+test('should return MongoConf Object if JSON is right', (t) => {
 	t.plan(1)
 	pipe(
 		parseConf(validEnv),
